5c16635264680da0b9db0c4cbc06df14ead18e5e
Bekinston <bektalgat1@gmail.com>
2020-10-29 11:24:39 +0600

    Updates

--- precache-manifest.c9aa7c5a038dea2ce56ac796b180ccd6.js
+++ precache-manifest.c9aa7c5a038dea2ce56ac796b180ccd6.js
@@ -1,39 +1,43 @@
 self.__precacheManifest = [
   {
-    "revision": "3bf51fd7d39b5b40fab7",
-    "url": "/b2.github.io/static/js/main.99a5fae2.chunk.js"
+    "revision": "28dc54758cb43e99e2d7",
+    "url": "/b2.github.io/static/js/main.1240e1c7.chunk.js"
   },
   {
     "revision": "c70420e2b667773619f6",
     "url": "/b2.github.io/static/js/runtime~main.1b94bd49.js"
   },
   {
-    "revision": "ba0a4471f8a006b679fa",
+    "revision": "c8b9056fb38ff464883a",
     "url": "/b2.github.io/static/css/2.ad321462.chunk.css"
   },
   {
-    "revision": "ba0a4471f8a006b679fa",
-    "url": "/b2.github.io/static/js/2.79a4617c.chunk.js"
+    "revision": "c8b9056fb38ff464883a",
+    "url": "/b2.github.io/static/js/2.96704166.chunk.js"
   },
   {
     "revision": "4c6eb969fead9db711f26bb8101c2c40",
     "url": "/b2.github.io/static/media/ug.4c6eb969.png"
   },
   {
-    "revision": "90a1a2a528cfb969c2a18ae45829dfa4",
-    "url": "/b2.github.io/static/media/coru.90a1a2a5.png"
-  },
-  {
-    "revision": "833e5526be4849de0dc3dc15b239ce29",
-    "url": "/b2.github.io/static/media/cozn.833e5526.png"
-  },
-  {
     "revision": "725110ad819ce9f3b8ca0d3340a0e64d",
     "url": "/b2.github.io/static/media/au.725110ad.png"
   },
   {
+    "revision": "90a1a2a528cfb969c2a18ae45829dfa4",
+    "url": "/b2.github.io/static/media/coru.90a1a2a5.png"
+  },
+  {
+    "revision": "298b1d11038d85de12b8fc6342e707f8",
+    "url": "/b2.github.io/static/media/checkall.298b1d11.png"
+  },
+  {
     "revision": "8a242f005ba524caf0a46ee0a2beac02",
     "url": "/b2.github.io/static/media/el2.8a242f00.png"
+  },
+  {
+    "revision": "833e5526be4849de0dc3dc15b239ce29",
+    "url": "/b2.github.io/static/media/cozn.833e5526.png"
   },
   {
     "revision": "e823e2b5b7989a9a1aed3c0195bd6709",
@@ -44,27 +48,23 @@ self.__precacheManifest = [
     "url": "/b2.github.io/static/media/pro.affb129c.png"
   },
   {
-    "revision": "48233834f5e747f94bb2406bb6b7faf4",
-    "url": "/b2.github.io/static/media/rails.48233834.png"
-  },
-  {
     "revision": "54725e1fd620c047b97f1ec9f71bf6ec",
     "url": "/b2.github.io/static/media/re.54725e1f.png"
-  },
-  {
-    "revision": "d27ae686205cea1e84a9541a2d09a828",
-    "url": "/b2.github.io/static/media/roads.d27ae686.png"
   },
   {
     "revision": "89bd8f7b98935e64022ccf9fafc36e74",
     "url": "/b2.github.io/static/media/vo.89bd8f7b.png"
   },
   {
-    "revision": "298b1d11038d85de12b8fc6342e707f8",
-    "url": "/b2.github.io/static/media/checkall.298b1d11.png"
+    "revision": "48233834f5e747f94bb2406bb6b7faf4",
+    "url": "/b2.github.io/static/media/rails.48233834.png"
   },
   {
-    "revision": "764e0cfb14334c387a2707bb5480486d",
+    "revision": "d27ae686205cea1e84a9541a2d09a828",
+    "url": "/b2.github.io/static/media/roads.d27ae686.png"
+  },
+  {
+    "revision": "314189f29bc7eddba5a994848399c679",
     "url": "/b2.github.io/index.html"
   }
 ];
